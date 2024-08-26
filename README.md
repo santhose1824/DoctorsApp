7d9784cd5e8081ca6fc9e8868c17566e06eab472
santhose1824 <santhosemaha18@gmail.com>
2024-08-26 21:48:29 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# :hospital: Doctors Mobile App 
+
+## :package: Packages
+- Sizer - https://pub.dev/packages/sizer
+- Table_Calendar - https://pub.dev/packages/table_calendar
+- Unicons - https://pub.dev/packages/unicons
+- Google_Fonts - https://pub.dev/packages/google_fonts
+- Cached_Network_Image - https://pub.dev/packages/cached_network_image
+
+## :hammer_and_wrench: New Widgets/Stuff that I tried
+- Sizer
+- Spacer
+
+##  :art: Design
+- The design is inspired by Unflip
+  - https://dribbble.com/shots/14277407-Medical-Mobile-App
+- Font Family: Yantramanav
+- Images are from:
+  - www.unsplash.com
+  - www.pexels.com
+
+## :bulb: Challenges
+- Making sure that the contrast ratio between background and text is good :boom:
+- Deciding which color combination to use for the Scaffold (background-color) :boom:
+
+## Screenshot
+<img width="1920" alt="DoctorApp" src="https://user-images.githubusercontent.com/44742460/140801921-3144c61c-406a-4439-ac29-abc215ba2643.png">
+
+## Live preview
+https://user-images.githubusercontent.com/44742460/140803113-e929d03a-2241-4bdb-be38-683b92b9118d.mp4
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
+import 'utils/custom_nav_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'DoctorsApp',
+          theme: buildThemeData(),
+          home: const CustomNavBar(),
+        );
+      },
+    );
+  }
+
+  ThemeData buildThemeData() {
+    return ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+            headline1: GoogleFonts.yantramanav(
+              fontSize: 22.0.sp,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.0,
+              color: Colors.black87,
+            ),
+            headline4: GoogleFonts.yantramanav(
+              fontSize: 20.0.sp,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.0,
+              color: Colors.black87,
+            ),
+            headline3: GoogleFonts.yantramanav(
+              fontSize: 16.0.sp,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.0,
+              color: Colors.black87,
+            ),
+            headline2: GoogleFonts.yantramanav(
+              fontSize: 18.0.sp,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.0,
+              color: Colors.black87,
+            ),
+            bodyText1: GoogleFonts.yantramanav(
+              fontSize: 12.0.sp,
+              letterSpacing: 1.0,
+              fontWeight: FontWeight.w400,
+            ),
+            bodyText2: GoogleFonts.yantramanav(
+                fontSize: 14.0.sp, letterSpacing: 1.0),
+            headline5: GoogleFonts.yantramanav(
+                fontSize: 16.0.sp, letterSpacing: 1.0),
+          ),
+          splashColor: const Color(0xff1651DA),
+        );
+  }
+}
